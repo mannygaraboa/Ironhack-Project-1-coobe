3dc9479ca484d23148b8dd4be42d17ece3156a83
Manuel Garaboa <mannygaraboa@gmail.com>
2019-02-04 15:59:05 -0500

    First moving obstacle

--- coobe.js
+++ coobe.js
@@ -1,55 +1,115 @@
 //To start the game
 window.onload = function() 
 {
-    document.getElementById("start-button").onclick = function() 
-    {
+    //document.getElementById("start-button").onclick = function() 
+    //{
         startGame();
-    };
+    //};
 }
 
 function startGame()
 {
     let canvas = document.getElementById('grid');
     let ctx = canvas.getContext('2d');
+    //document.getElementById().blur;
 
     canvas.width = 800;
     canvas.height = 300;
-
-    function drawGrid()
+    
+    let coobe = 
     {
-
-    }
-
+        x: 100,
+        y: 270
+    };
+    
     function drawCoobe()
     {
-
+        ctx.fillStyle = 'black';
+        ctx.fillRect(coobe.x, coobe.y, 30, 30)
     }
-
-    class coobe
+    
+    var obstacle = 
     {
-        constructor(x,y)
-        {
-            this.x = x;
-            this.y = y;
-        }
+        x: 850,
+        y: 300
     }
-    let coobe = new coobe(0,0);
-
-    var obstacles
 
     function drawObstacles()
     {
+        
 
+        ctx.beginPath();
+        ctx.moveTo(obstacle.x,obstacle.y);
+        ctx.lineTo(obstacle.x + 15, obstacle.y - 25);
+        ctx.lineTo(obstacle.x + 30, obstacle.y);
+        ctx.fill();
+        obstacle.x -= 5;
+        // ctx.beginPath();
+        // ctx.moveTo(700,300);
+        // ctx.lineTo(715,275);
+        // ctx.lineTo(730,300);
+        // ctx.fill();
+        //ctx.fillRect(obstacle.x, obstacle.y, 30, 30);
     }
 
-    function collision()
-    {
+    // qbe = 
+    // {
+    //     x : 0,
+    //     y : 0, 
+    //     speed: 0,2
+    //     size: 20,
+    //     onFloor: false,
+    //     drag: 0.99,
+    //     jumpPower: -10
+    // }
+    // const gravity = 0.1;
 
-    }
+    // cuebe.speed += gravity;
+    // cuebe.y += cuebe.speed;
+
+    // if(cuebe.y + cuebe.size >= 150)
+    // {
+    //     cuebe.y = 150 - cuebe.size;
+    //     cuebe.speed = 0;
+    //     cuebe.onFloor = true;
+    // }
+    // else
+    // {
+    //     cuebe.onFloor = false;
+    // }
+
+    // class Coobe
+    // {
+    //     constructor(x,y)
+    //     {
+    //         this.x = x;
+    //         this.y = y;
+
+    //         jump()
+    //         {
+    //             this.y -= 30;
+    //         }
+    //     }
+    // }
+    // let coobe = new Coobe(100,270)
+    
+
+    // function drawObstacles()
+    // {
+
+    // }
+
+    // function collision()
+    // {
+
+    // }
             
-    document.spacebar = function(e) 
+    document.body.onkeydown = function(e) 
     {
-
+        if(e.keyCode == 32 && coobe.onFloor)
+        {
+            coobe.jump();
+        }
     }
 
 
@@ -58,10 +118,9 @@ function startGame()
         window.requestAnimationFrame(animate);
         //draw everything and erase everything
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawGrid();
         drawCoobe();
         drawObstacles();
-        collision();
+        //collision();
     }
     animate();
 }
