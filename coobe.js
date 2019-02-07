2c74fd001574399c646ad216630f87924c7ea528
Manuel Garaboa <mannygaraboa@gmail.com>
2019-02-07 18:33:55 -0500

    Infinite jump glitch is gone

--- coobe.js
+++ coobe.js
@@ -12,7 +12,6 @@ function music()
     }, 1)
   var audio = new Audio("Synthetic Life.mp3")
   audio.play();
-
 }
 
 let coobe = 
@@ -24,12 +23,14 @@ let coobe =
     jumpPressed: false,
     //jumpLimit: 
     color: "rgb(23, 234, 167)",
-    falling: false
+    falling: false,
+    jump: 180,
+    onSomething: true
 };
 
 let spikes = [];
-let squares = [];
-let platforms = [];
+let groundSquares = [];
+let airSquares = [];
 
 let gameOver  = false; 
 let canvas = document.getElementById('grid');
@@ -46,14 +47,21 @@ window.onload = function()
     };
 }
 
-    function startGame()
-    {
-        music();
-        animate();
-        // setInterval(animate, 1)
-    }
+function startGame()
+{
+    music();
+    animate();
+}
 
-//let drag = 0;
+// color: "rgb(23, 234, 167)"
+// falling: false
+// height: 30
+// jump: 180
+// jumpPressed: true
+// onSomething: false
+// width: 30
+// x: 100
+// y: 193
 function drawCoobe()
 {
     ctx.beginPath();
@@ -63,41 +71,62 @@ function drawCoobe()
     ctx.strokeRect(coobe.x, coobe.y, coobe.width, coobe.height, "rgb(0, 221, 255)")
     ctx.closePath();
     coobe.falling = false;
-
-    if(coobe.jumpPressed)
+    console.log(coobe)
+    let ground = 270
+    if(coobe.jumpPressed && coobe.onSomething) //Jumping up 
     {
-        coobe.y -= 8;
+        coobe.y -= 7 
         coobe.falling = false;
+       // coobe.jumpPressed = false;
+
     }
-    if(!coobe.jumpPressed && coobe.y < 270)
+    if(!coobe.jumpPressed && coobe.y < 269) //Falling until hits the ground
     {
-        coobe.y += 8;
+        coobe.jump = 180
+        coobe.y += 7;
         coobe.falling = true;
+        coobe.jumpPressed = false
+        //coobe.onSomething = true; 
+
     }
-    if(coobe.y < 180)
+    if(coobe.y > 269){
+       coobe.onSomething = true;
+       coobe.jumpPressed = false;
+    }
+    //console.log(coobe.y, obstacle2.height)
+    //let currentHeight = coobe.y; 
+    //console.log(coobe.jump)
+    if(coobe.y < (coobe.jump)) //Reach his jump limit
     {
         coobe.jumpPressed = false;
+        coobe.onSomething = false; 
+
     }
 }
 
 
-function jump()
-{
-    coobe.jumpPressed === true; // As soon as spacebar is pressed
-}
+// function jump()
+// {   
+//     coobe.jumpPressed === true; // As soon as spacebar is pressed
+//     //coobe.jump = 180; 
+
+//     //setInterval(function ()
+// }
 document.onkeydown = function(e) 
 {
     if(e.keyCode == 32)//&& coobe.y == 270)
     {
-        jump();
+        //jump();
         beats.push(frames);
-        console.log("beat");
-        coobe.jumpPressed = true;
+        //console.log("beat");
+        if(coobe.onSomething){
+            coobe.jumpPressed = true;
+        }
     }
     else
     {
         coobe.jumpPressed = false;
-    }
+    } 
 }
 
 
@@ -106,8 +135,8 @@ var obstacle1 =
 {
     x: 850,
     y: 300,
-    width: 25,
-    height: 25,
+    width: 20,
+    height: 20,
 }
 function drawOneSpike(obstacle1)
 {
@@ -127,7 +156,10 @@ function drawSpikes()
     }
 }
 // Map of Spikes
-let spikeBeatles = [149, 152, 312, 387, 473, 551, 628, 706, 788, 829, 869, 909, 948, 1023, 1065, 1106, 1186, 1223, 1261, 1332, 1375, 1414, 1420, 1463, 1543, 1581, 1584, 1640, 1699, 1766, 1772, 1849, 1852, 1866, 1930, 1973, 2013, 2049, 2091, 2127, 2196, 2199, 2315, 2318, 2429, 2432, 2533, 2536, 2631, 2634, 2713, 2756, 2796, 2865, 2871, 2942, 2949, 3019, 3026, 3101, 3143, 3184, 3295, 3334, 3375, 3415, 3447, 3452, 3495, 3532, 3571, 3613, 3653, 3692, 3729, 3735, 3769, 3809, 3847, 3878, 3884, 3928, 3963, 4004, 4041, 4047, 4083, 4122, 4161, 4197, 4203, 4239, 4277, 4315, 4354, 4429, 4505, 4549, 4585, 4660, 4666];
+let spikeBeatles = 
+[162, 314, 439, 554, 668, 788, 854, 904, 947, 986, 1027, 1068, 1109
+//156, 314, 317, 384, 387, 390, 436, 551, 638, 717, 792, 868, 908, 947
+];
 createSpikes()
 function createSpikes()
 {
@@ -135,11 +167,11 @@ function createSpikes()
     {
         spikes.push({...obstacle1, x:spikeBeatles[i]*10+150})
     }
-    console.log(spikes);
+    //console.log(spikes);
 }
 
 
-// The Squares
+// The Ground Squares
 var obstacle2 = 
 {
     x: 850,
@@ -156,32 +188,35 @@ function drawOneSquare(obstacle2)
     ctx.closePath();
     obstacle2.x -= 10;
 }
-function drawSquares()
+function drawGroundSquares()
 {
-    for(let square of squares)
+    for(let groundSquare of groundSquares)
     {
-        drawOneSquare(square)
+        drawOneSquare(groundSquare)
     }
 }
 // Map of Squares
-let squareBeatles = [200, 400, 600, 800, 1000, 1200, 1400, 1600];
+let groundSquareBeatles = 
+[
+381, 384, 387, 390, 393, 396, 399, 402, 405
+];
 createSquares()
 function createSquares()
 {
-    for(let i = 0; i < squareBeatles.length; i++)
+    for(let i = 0; i < groundSquareBeatles.length; i++)
     {
-        squares.push({...obstacle2, x:squareBeatles[i]*10+150})
+        groundSquares.push({...obstacle2, x:groundSquareBeatles[i]*10+150})
     }
-    console.log(squares);
+    //console.log(groundSquares);
 }
 
 
-// The Platforms
+// The Air Squares
 var obstacle3 = 
 {
     x: 850,
     y: 250,
-    width: 250,
+    width: 30,
     height: 30
 }
 function drawOnePlatform(obstacle3)
@@ -193,23 +228,26 @@ function drawOnePlatform(obstacle3)
     ctx.closePath();
     obstacle3.x -= 10;
 }
-function drawPlatforms()
+function drawAirSquares()
 {
-    for(let platform of platforms)
+    for(let airSquare of airSquares)
     {
-        drawOnePlatform(platform)
+        drawOnePlatform(airSquare)
     }
 }
 // Map of Platforms
-let platformBeatles = [350, 450, 550, 650, 850, 900, 910];
+let airSquareBeatles = 
+[
+400, 403, 406, 409, 412, 415, 418, 421, 424, 427, 430
+];
 createPlatforms()
 function createPlatforms()
 {
-    for(let i = 0; i < platformBeatles.length; i++)
+    for(let i = 0; i < airSquareBeatles.length; i++)
     {
-        platforms.push({...obstacle3, x:platformBeatles[i]*10+150});
+        airSquares.push({...obstacle3, x:airSquareBeatles[i]*10+150});
     }
-    console.log(platforms);
+    //console.log(airSquares);
 }
 
 
@@ -219,35 +257,33 @@ function collision()
     for(let spike of spikes)
     {
         if 
-        (coobe.x < spike.x + spike.width &&
+        (coobe.x < (spike.x + spike.width) &&
         coobe.x + coobe.width > spike.x &&
         coobe.y < spike.y &&
         coobe.y + coobe.height > spike.y - 30) 
         {
-            console.log("collided")
+            //console.log("collided")
 
             gameOver = true; 
             // collision detected!
         }
-            
     }
 
-    for(let square of squares)
+    for(let groundSquare of groundSquares)
     {
         if 
-        (coobe.x < square.x + square.width &&
-        coobe.x + coobe.width > square.x &&
-        coobe.y < square.y + square.height &&
-        coobe.y + coobe.height > square.y) 
+        (coobe.x < groundSquare.x + groundSquare.width &&
+        coobe.x + coobe.width > groundSquare.x &&
+        coobe.y < groundSquare.y + groundSquare.height &&
+        coobe.y + coobe.height > groundSquare.y) 
         {
-
+            coobe.jump = 180 - groundSquare.height//groundSquare.height + 30;
             if(coobe.falling)
             {
-                console.log("on platform")
-                coobe.y = square.y - square.height
+                //console.log("on platform")
+                coobe.y = (groundSquare.y - groundSquare.height);
                 coobe.falling = false;
-                //debugger
-                //coobe.y = coobe.y - (square.height)
+                coobe.onSomething = true; 
             }
             else
             {
@@ -258,20 +294,23 @@ function collision()
         } 
     }
 
-    for(let platform of platforms)
+    for(let airSquare of airSquares)
     {
         if 
-        (coobe.x < platform.x + platform.width &&
-        coobe.x + coobe.width > platform.x &&
-        coobe.y < platform.y + platform.height &&
-        coobe.y + coobe.height > platform.y) 
+        (coobe.x < airSquare.x + airSquare.width &&
+        coobe.x + coobe.width > airSquare.x &&
+        coobe.y < airSquare.y + airSquare.height &&
+        coobe.y + coobe.height > airSquare.y) 
         {
+            coobe.jump = 180 - airSquare.height //airSquare.height + 30; 
 
             if(coobe.falling)
             {
-                console.log("on platform")
-                coobe.y = platform.y - platform.height
+                //console.log("on platform")
+                coobe.y = (airSquare.y - airSquare.height);
                 coobe.falling = false;
+                coobe.onSomething = true; 
+
                 //debugger
                 //coobe.y = coobe.y - (square.height)
             }
@@ -295,7 +334,7 @@ function animate()
         setTimeout(function() 
         {
             window.location.reload()
-        }, 1000)
+        }, 250)
         
         return;
     }
@@ -305,8 +344,8 @@ function animate()
     drawCoobe();
     //drawOneSpike();
     drawSpikes();
-    drawSquares();
-    drawPlatforms();
+    drawGroundSquares();
+    drawAirSquares();
     collision();
     frames++; 
 }
